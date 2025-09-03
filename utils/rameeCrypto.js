782dcfd6a0231b49065be1249810b539df6fbc8f
unknown <chandanpms@gmail.com>
2025-09-03 14:59:17 +0530

    updated

--- utils/rameeCrypto.js
+++ utils/rameeCrypto.js
@@ -3,16 +3,11 @@ const crypto = require("crypto");
 require("dotenv").config();
 
 const SECRET_KEY = process.env.RAMEEPAY_SECRET_KEY; // must be 32 bytes
-const SECRET_IV = process.env.RAMEEPAY_SECRET_IV; // provided by Ramee (must be 12 or 16 bytes depending on config)
+// const SECRET_IV = process.env.RAMEEPAY_SECRET_IV;
 
 // AES-256-GCM encryption
 function encryptData(data) {
-  const iv = Buffer.from(SECRET_IV, "utf8"); // use fixed IV from dashboard
-  const cipher = crypto.createCipheriv(
-    "aes-256-gcm",
-    Buffer.from(SECRET_KEY),
-    iv
-  );
+  const cipher = crypto.createCipheriv("aes-256-gcm", Buffer.from(SECRET_KEY));
 
   const encrypted = Buffer.concat([
     cipher.update(JSON.stringify(data), "utf8"),
@@ -20,21 +15,19 @@ function encryptData(data) {
   ]);
   const tag = cipher.getAuthTag();
 
-  return Buffer.concat([iv, tag, encrypted]).toString("base64");
+  return Buffer.concat([tag, encrypted]).toString("base64");
 }
 
 // AES-256-GCM decryption
 function decryptData(encryptedData) {
   const bData = Buffer.from(encryptedData, "base64");
-
-  const iv = bData.slice(0, 12); // fixed IV length
+  // fixed IV length
   const tag = bData.slice(12, 28); // 16-byte tag
   const text = bData.slice(28);
 
   const decipher = crypto.createDecipheriv(
     "aes-256-gcm",
-    Buffer.from(SECRET_KEY),
-    iv
+    Buffer.from(SECRET_KEY)
   );
   decipher.setAuthTag(tag);
 
