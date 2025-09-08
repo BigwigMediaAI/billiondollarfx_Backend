3ae462b72ac6885b4ec1536fba21663774857e29
unknown <chandanpms@gmail.com>
2025-09-08 17:31:33 +0530

    updated

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
+    account: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+    },
     orderid: { type: String, required: true, unique: true },
     accountNo: { type: String, required: true },
     amount: { type: Number, required: true },
